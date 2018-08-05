7611b64fee7ab6ac110cc40e63c0707b79f8e2ec
sayemon10 <abdulaziz.aziz99@hmail.com>
2018-08-05 18:29:31 +0600

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {    
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 500) {
@@ -10,7 +10,7 @@ $(window).scroll(function() {
 
 //Preloader
 var preloader = $('.preloader');
-$(window).on('load', function() {
+$(window).on('load', function () {
     var preloaderFadeOutTime = 300;
 
     function hidePreloader() {
