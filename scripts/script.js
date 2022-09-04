e0435af54b352c5a7f74fe967710c11b0b462c76
sayemon10 <abdulaziz.aziz99@gmail.com>
2022-09-05 04:30:53 +0600

    Add: Experience section & twisting text

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
+// let colors = ["#F15121", "#7EB801", "#00A3EE", "#FFB700"];
 let colors = ["#24d05a", "#eb4888", "#10a2f5", "#e9bc3f"];
 
 (function () {
+  splitCharacters();
   setModeEventListener();
   setRandomLinkColor();
   setColorHoverListener();
@@ -15,6 +17,55 @@ let colors = ["#24d05a", "#eb4888", "#10a2f5", "#e9bc3f"];
     setRandomLinkColor();
   }, 9000);
 })();
+
+// twisting text
+function splitCharacters() {
+  const targets = document.querySelectorAll("[data-split]");
+
+  for (const target of targets) {
+    let string = '<span class="inner"><span class="front">';
+    let counter = 0;
+
+    const targetContent = target.textContent;
+    const words = targetContent.trim().split(" ");
+
+    words.forEach(function (word, wordIndex, wordArray) {
+      const chars = word.split("");
+      chars.forEach(function (char, charIndex, charArray) {
+        string += `<span class="char" style="--index: ${++counter};">${char}</span>`;
+
+        // if we're on the last character of the last word, reset the counter
+        if (
+          wordIndex === wordArray.length - 1 &&
+          charIndex === charArray.length - 1
+        ) {
+          counter = 0;
+        }
+      });
+
+      // add a space between each word unless it's the last one
+      if (wordIndex !== wordArray.length - 1) {
+        string += "<span>&nbsp;</span>";
+      }
+    });
+    string += "</span>"; //end front
+
+    string += '<span class="back">';
+    words.forEach(function (word, wordIndex, wordArray) {
+      const chars = word.split("");
+      chars.forEach(function (char) {
+        string += `<span class="char" style="--index: ${++counter};">${char}</span>`;
+      });
+      if (wordIndex !== wordArray.length - 1) {
+        string += "<span>&nbsp;</span>";
+      }
+    });
+
+    string += "</span>"; // end back
+    string += "</span>"; // end inner
+    target.innerHTML = string;
+  }
+}
 
 /* Dark Mode */
 function setModeEventListener() {
